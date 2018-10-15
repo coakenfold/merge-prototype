7e5947d49b56ff2faf84ab9ad1763821e39f1a6f
Chad Oakenfold <web@oakenfold.ca>
2018-10-14 21:08:03 -0700

    Externalized propType definition

--- src/components/MergeRecords/MergeRecords.js
+++ src/components/MergeRecords/MergeRecords.js
@@ -1,84 +1,17 @@
 import React, { PureComponent } from "react";
-import { array, arrayOf, bool, shape, string } from "prop-types";
+
 import "./MergeRecords.css";
 import uuid from "uuid/v4";
-
+import { arrayOf } from "prop-types";
 import Record from "../Record/Record";
-
+import dataPropType from "../dataPropType";
 class MergeRecords extends PureComponent {
   state = {
     merged: null,
     needsReview: []
   };
   static propTypes = {
-    data: arrayOf(
-      shape({
-        uid: string.isRequired,
-        type: string,
-        word: string,
-        part_of_speech: string,
-        categories: arrayOf(
-          shape({
-            uid: string.isRequired,
-            "dc:title": string
-          })
-        ),
-        sources: arrayOf(
-          shape({
-            uid: string.isRequired,
-            "dc:title": string
-          })
-        ),
-        related_phrases: arrayOf(
-          shape({
-            uid: string.isRequired,
-            "fv:definitions": arrayOf(
-              shape({
-                language: string,
-                translation: string
-              })
-            ),
-            "fv:literal_translation": array,
-            phrase: string
-          })
-        ),
-        related_audio: arrayOf(
-          shape({
-            uid: string.isRequired,
-            name: string,
-            "mime-type": string,
-            path: string,
-            "dc:title": string,
-            "dc:description": string
-          })
-        ),
-        related_pictures: arrayOf(
-          shape({
-            uid: string.isRequired,
-            name: string,
-            "mime-type": string,
-            path: string,
-            "dc:title": string,
-            "dc:description": string
-          })
-        ),
-        lastModified: string,
-        "fv:reference": string,
-        "fv:definitions": arrayOf(
-          shape({
-            translation: string,
-            language: string
-          })
-        ),
-        "fv:cultural_note": arrayOf(string),
-        "fv:available_in_childrens_archive": bool,
-        "dc:creator": string,
-        "dc:lastContributor": string,
-        "dc:contributors": arrayOf(string),
-        "fv-word:pronunciation": string,
-        "fv-word:part_of_speech": string
-      })
-    )
+    data: arrayOf(dataPropType)
   };
   componentDidMount() {
     this._mergeRecords();
--- src/components/Record/Record.js
+++ src/components/Record/Record.js
@@ -1,76 +1,12 @@
 import React, { PureComponent } from "react";
-import { array, arrayOf, bool, func, shape, string } from "prop-types";
+import { func } from "prop-types";
 import "./Record.css";
+import dataPropType from "../dataPropType";
 
 class Record extends PureComponent {
   static propTypes = {
     setAlternate: func,
-    data: shape({
-      uid: string.isRequired,
-      type: string,
-      word: string,
-      part_of_speech: string,
-      categories: arrayOf(
-        shape({
-          uid: string.isRequired,
-          "dc:title": string
-        })
-      ),
-      sources: arrayOf(
-        shape({
-          uid: string.isRequired,
-          "dc:title": string
-        })
-      ),
-      related_phrases: arrayOf(
-        shape({
-          uid: string.isRequired,
-          "fv:definitions": arrayOf(
-            shape({
-              language: string,
-              translation: string
-            })
-          ),
-          "fv:literal_translation": array,
-          phrase: string
-        })
-      ),
-      related_audio: arrayOf(
-        shape({
-          uid: string.isRequired,
-          name: string,
-          "mime-type": string,
-          path: string,
-          "dc:title": string,
-          "dc:description": string
-        })
-      ),
-      related_pictures: arrayOf(
-        shape({
-          uid: string.isRequired,
-          name: string,
-          "mime-type": string,
-          path: string,
-          "dc:title": string,
-          "dc:description": string
-        })
-      ),
-      lastModified: string,
-      "fv:reference": string,
-      "fv:definitions": arrayOf(
-        shape({
-          translation: string,
-          language: string
-        })
-      ),
-      "fv:cultural_note": arrayOf(string),
-      "fv:available_in_childrens_archive": bool,
-      "dc:creator": string,
-      "dc:lastContributor": string,
-      "dc:contributors": arrayOf(string),
-      "fv-word:pronunciation": string,
-      "fv-word:part_of_speech": string
-    })
+    data: dataPropType
   };
   componentDidMount() {}
   render() {
