0ac763caae63f3333ddb59211f62ad19987b8578
Chad Oakenfold <web@oakenfold.ca>
2018-10-14 21:24:53 -0700

    xhr via axios

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
 import React, { Component } from "react";
 import "./App.css";
 
-// TODO: consume via xhr
-import data from "./data/data";
-
+// import dummyData from "./data/data";
+import axios from "axios";
 import MergeRecords from "./components/MergeRecords/MergeRecords";
 class App extends Component {
+  state = {
+    data: null,
+    error: null
+  };
+  componentDidMount() {
+    // Make a request for a user with a given ID
+    axios
+      .get("https://pznmh01oo9.execute-api.ca-central-1.amazonaws.com/dev/test-merge-two-records")
+      .then(response => {
+        this.setState({ data: response.data });
+        // this.setState({ data: dummyData });
+      })
+      .catch(error => {
+        this.setState({ error });
+      });
+  }
   render() {
+    const { data, error } = this.state;
+    let content = null;
+    if (error) {
+      content = <div>ERROR!</div>;
+    }
+    if (data) {
+      content = <MergeRecords data={data} />;
+    }
     return (
       <div className="App">
         <h1>Prototype</h1>
-        <MergeRecords data={data} />
+        {content}
       </div>
     );
   }
